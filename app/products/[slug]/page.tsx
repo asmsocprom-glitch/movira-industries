2aaf672bb24e6e7f2e0fff36ed36513082679897
Sidhesh-02 <sidheshcreates@gmail.com>
2025-11-19 15:14:33 +0530

    slug-problem-2

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -3,14 +3,14 @@ import { notFound } from "next/navigation";
 import products from "@/app/lib/product.json";
 import ProductContent from "./ProductContent";
 
-interface ProductPageProps {
-  params: { slug: string };
-}
+export default async function ProductPage({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params; // ← FIX
 
-export default async function ProductPage({ params }: ProductPageProps) {
-  const { slug } = await params;
   const product = products.find((p) => p.slug === slug);
-
   if (!product) return notFound();
 
   return <ProductContent product={product} />;
